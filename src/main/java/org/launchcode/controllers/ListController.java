6ddbbfc35f02385d1d2da54616a6e43bda5a81d9
Jonny McFadden <jmcfadden1528@gmail.com>
2018-06-02 10:27:58 -0500

    form points to results page correctly. trying to get for loop to display proper jobs.

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 @RequestMapping(value = "list")
 public class ListController {
 
-    static HashMap<String, String> columnChoices = new HashMap<>();
+    static HashMap<String, String> columnChoices = new HashMap<>(); //CONSTRUCTOR USED TO POPULATE COLUMN CHOICES
 
     public ListController () {
         columnChoices.put("core competency", "Skill");
@@ -29,32 +29,31 @@ public class ListController {
     @RequestMapping(value = "")
     public String list(Model model) {
 
-        model.addAttribute("columns", columnChoices);
+        model.addAttribute("columns", columnChoices); //DISPLAYS CHOICES USER CAN VIEW
 
         return "list";
     }
 
     @RequestMapping(value = "values")
-    public String listColumnValues(Model model, @RequestParam String column) {
+    public String listColumnValues(Model model, @RequestParam String column) { //QUERIES THE COLUMN USER SELECTED
 
         if (column.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
             model.addAttribute("jobs", jobs);
-            return "list-jobs";
+            return "list-jobs"; //RETURNS ALL JOBS TO USER
         } else {
             ArrayList<String> items = JobData.findAll(column);
             model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
             model.addAttribute("column", column);
             model.addAttribute("items", items);
-            return "list-column";
+            return "list-column"; //RETURNS SELECTED DATA REGARDING JOBS TO USER
         }
 
     }
 
     @RequestMapping(value = "jobs")
-    public String listJobsByColumnAndValue(Model model,
-            @RequestParam String column, @RequestParam String value) {
+    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam String value) { //QUERIES COLUMN AND VALUE SELECTED BY USER
 
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,11 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.ArrayList;
-import java.util.HashMap;
+
+
 
 /**
  * Created by LaunchCode
@@ -24,4 +25,21 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+
+    @RequestMapping(value = "results", method = RequestMethod.POST)
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<String> jobs = JobData.findAll(searchType);
+        ArrayList<String> jobResults = new ArrayList<>();
+        for (String jobResult : jobs) {
+            if (jobResult.contains(searchTerm)) {
+                jobResults.add(jobResult);
+            }
+        }
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("jobResults", jobResults);
+
+        return "results";
+    }
 }
